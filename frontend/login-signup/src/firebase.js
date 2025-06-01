daaa66c01b1d5fb22851ebee2f26f1a27f66246b
RezEnayati <rezareza4302@gmail.com>
2025-05-31 20:04:26 -0400

    Refactored Firebase config to use environment variables

--- frontend/login-signup/src/firebase.js
+++ frontend/login-signup/src/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 
 // I put the config in the discord for easy setup - REZA
 const firebaseConfig = {
-    apiKey: "AIzaSyAN1C-3Cvk2uDSZFFPxlYz9REG8vvZGTGE",
+    apiKey: "REPLACE WITH API KEY",
     authDomain: "comp380-project.firebaseapp.com",
     projectId: "comp380-project",
     storageBucket: "comp380-project.firebasestorage.app",
     messagingSenderId: "600625641164",
-    appId: "1:600625641164:web:08ae615ac03593930d7469",
+    appId: "REPLACE WITH APP ID",
     measurementId: "G-Z4CSFDJFNB"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
