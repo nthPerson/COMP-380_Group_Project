ed909d5f452895088c7eea500c8b5dfbb5423f4a
robert <robertashe2.0@gmail.com>
2025-06-03 23:34:11 -0700

    Set up .env file for firebase.js secrets

--- frontend/login-signup/src/firebase.js
+++ frontend/login-signup/src/firebase.js
@@ -3,15 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 
-// I put the config in the discord for easy setup - REZA
+// Environment file should contain the following variables at COMP-380_Group_Project/frontend/login-signup/.env
+// See Discord > #configs text channel for details on variable values
 const firebaseConfig = {
-  apiKey: "AIzaSyAN1C-3Cvk2uDSZFFPxlYz9REG8vvZGTGE",
-  authDomain: "comp380-project.firebaseapp.com",
-  projectId: "comp380-project",
-  storageBucket: "comp380-project.firebasestorage.app",
-  messagingSenderId: "600625641164",
-  appId: "1:600625641164:web:08ae615ac03593930d7469",
-  measurementId: "G-Z4CSFDJFNB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
