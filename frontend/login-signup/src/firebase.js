a7797908e93dc8580e2e4ebf0f66b0fc344b951e
RezEnayati <rezareza4302@gmail.com>
2025-06-04 00:38:32 -0700

    SCRUM-34: Implemented forgot password functionality and moved firebase logic to services

--- frontend/login-signup/src/firebase.js
+++ frontend/login-signup/src/firebase.js
@@ -12,8 +12,8 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}; 
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
