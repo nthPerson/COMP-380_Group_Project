50cf91b82fc21d10da00d4a6167dc46938e7ca9c
robert <robertashe2.0@gmail.com>
2025-06-05 23:39:12 -0700

    Separated PDf upload logic out of app.py and into pdf_utils.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request, g
 from flask_cors import CORS #this enables us to make cross origin requests 
-from firebase_config import db, bucket  # this initializes Firebase & gives us the ... - @nthPerson testing cloud storage for PDF uploads
-from firebase_admin import firestore # Using to record timestamp of PDF upload
+from firebase_config import db  # this initializes Firebase & gives us the
 from verify_token import verify_firebase_token #what we use to verify the token we made this file 
 from gemini_utils import explain_jd_with_gemini #use geminiiiiiiiiiiii
+
+from pdf_utils import upload_user_pdf
 
 
 app = Flask(__name__) #makes an instance of the flask app 
@@ -21,25 +22,12 @@ def receive_jd():
         return jsonify({"error": f"Gemini failed: {e}"}), 500 
     
 
+# Upload user PDF
 @app.route("/api/upload_pdf", methods=["POST"])
 @verify_firebase_token
 def upload_pdf():
-    if "file" not in request.files:
-        return jsonify({"error": "No file part"}), 400 # If the file component is missing from request, return fail code
-    file = request.files["file"]
-    if file.filename == "":
-        return jsonify({"error": "No selected file"}), 400 # If there isn't a file selected in the request, return fail code
-    
-    user_id = g.firebase_user["uid"] # Gets user ID (g.firebase_user) from verify_firebase_token wrapper
-    blob = bucket.blob(f"user_docs/{user_id}/{file.filename}") # Builds filename as user_docs/{user_id}/PDF_filename
-    blob.upload_from_file(file, content_type=file.content_type) # Uploads file received from frontend HTTP request to Firebase Cloud Storage bucket
+    return upload_user_pdf()
 
-    db.collection("users").document(user_id).collection("documents").add({ # Add new document to the Firestore database under path users/{user_id}/documents/
-        "fileName": file.filename,  # Stores the original filename of the PDF
-        "storagePath": blob.name,  # Stores the Firebase Cloud Storage path where the file was uploaded
-        "uploadedAt": firestore.SERVER_TIMESTAMP  # Stores the upload time, set by Firestore's server
-    })
-    return jsonify({"message": "File uploaded"}), 200  # If everything worked, returns message and success code
 
 #  start the server
 if __name__ == "__main__":
