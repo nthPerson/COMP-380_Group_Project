ee369a87e558ffb1f89403b787efe9ce631eb553
gtech29 <juan.rodriguez.517@my.csun.edu>
2025-06-16 11:26:10 -0700

    Enhance UI: Homepage layout, Sidebar redesign, AOS integration

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -1,47 +1,43 @@
-// src/Components/Sidebar/Sidebar.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import logo_icon from '../Assets/logo_icon.png';
+import logo_icon from "../Assets/logo_icon.png";
 import profile_icon from "../Assets/profile_icon.png";
-import archive_icon from '../Assets/library_icon.png';
+import archive_icon from "../Assets/library_icon.png";
 import magicwand_icon from "../Assets/magic-wand_icon.png";
 
 const Sidebar = ({ user }) => (
-  <div className="sidebar">
-    {/* Logo and Title at the Top */}
-    <Link to="/landingPage" style={{ textDecoration: "none" }}>
-    <div className="sidebar-header" style={{ marginBottom: "2rem", textAlign: "center" }}>
-      <img src={logo_icon} alt="Logo icon" className="sidebar-logo" style={{ width: "60px" }} />
-      <div className="sidebar-title" style={{ fontWeight: 700, fontSize: "1.5rem" }}>
+  <aside className="sidebar">
+    <Link to="/landingPage" className="sidebar-header">
+      <img src={logo_icon} alt="Logo" className="sidebar-logo" />
+      <div className="sidebar-title">
         <span className="light-blue">Rezu</span>
         <span className="solid-blue">Me</span>
       </div>
-    </div>
     </Link>
-    {/* Navigation */}
+
     <nav className="sidebar-nav">
       <ul>
-      <li>
-  <Link to="/userProfile" className="profile-link">
-    <img src={profile_icon} alt="Profile icon" />
-    <span>{user?.displayName || "User"}'s Profile</span>
-  </Link>
-</li>
         <li>
-          <Link to="/tailorResume" className="tailor-link">
-            <img src= {magicwand_icon} alt="Tailor icon" />
-            <span>Tailor Resume</span>
-            </Link>
+          <Link to="/userProfile" className="sidebar-link">
+            <img src={profile_icon} alt="Profile icon" />
+            <span>{user?.displayName || "User"}'s Profile</span>
+          </Link>
         </li>
         <li>
-          <Link to="/resumeArchive" className="archive-link">
-            <img src= {archive_icon} alt="File icon" />
+          <Link to="/tailorResume" className="sidebar-link">
+            <img src={magicwand_icon} alt="Tailor icon" />
+            <span>Tailor Resume</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/resumeArchive" className="sidebar-link">
+            <img src={archive_icon} alt="Archive icon" />
             <span>Resume Archive</span>
-            </Link>
+          </Link>
         </li>
       </ul>
     </nav>
-  </div>
+  </aside>
 );
 
 export default Sidebar;
