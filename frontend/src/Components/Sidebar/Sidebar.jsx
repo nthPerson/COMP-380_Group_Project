ffb3201965ab8269978f6a795f4d65c8c5c41029
Bella <isabella-francesca.felipe.663@my.csun.edu>
2025-06-20 21:58:18 -0700

    Add log out button to sidebar

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { handleSignout } from '../../services/authHandlers';
+
 import logo_icon from "../Assets/logo_icon.png";
 import profile_icon from "../Assets/profile_icon.png";
 import archive_icon from "../Assets/library_icon.png";
 import magicwand_icon from "../Assets/magic-wand_icon.png";
+import logout_icon from "../Assets/logout_icon.png";
 
-const Sidebar = ({ user }) => (
+const Sidebar = ({ user }) => {
+  const navigate = useNavigate();
+
+  const onClick = async (e) => {
+    e.preventDefault();      // in case you’re inside a form
+    try {
+      await handleSignout();
+      console.log("Logged out");
+      navigate("/", { replace: true });
+    } catch (err) {
+      console.error("Log out failed:", err);
+    }
+  };
+
+  return (
   <aside className="sidebar">
     <Link to="/landingPage" className="sidebar-header">
       <img src={logo_icon} alt="Logo" className="sidebar-logo" />
@@ -35,9 +52,15 @@ const Sidebar = ({ user }) => (
             <span>Resume Archive</span>
           </Link>
         </li>
+        <li>
+         <button type="button" className="sidebar-link" onClick={onClick} style={{background: "none", border: "none", alignItems: "center" }}>
+          <img src={logout_icon} alt="Log Out icon" />
+          <span>Logout</span>
+         </button>
+        </li>
       </ul>
     </nav>
   </aside>
 );
-
+  };
 export default Sidebar;
